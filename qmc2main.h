11850ae48a715c0a390d33e35c17745ca7a6f175
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2009-05-24 20:48:53 +0000

    imp: delay MAWS page load

--- qmc2main.h
+++ qmc2main.h
@@ -241,6 +241,7 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 #if defined(QMC2_SDLMAME) || defined(QMC2_MAME)
     void loadEmuInfoDB();
     void mawsLoadFinished(bool);
+    void mawsLoadStart();
 #endif
     void createFifo(bool logFifoCreation = TRUE);
     void recreateFifo();
