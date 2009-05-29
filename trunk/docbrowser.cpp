d66c2e07f18941cb4a15e3b10057290ae5b2ad8d
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2009-05-29 11:46:06 +0000

    new: save/restore layout of documentation browser

--- trunk/docbrowser.cpp
+++ trunk/docbrowser.cpp
@@ -1,11 +1,12 @@
+#include <QSettings>
+
 #include "docbrowser.h"
-#include "options.h"
 #include "qmc2main.h"
 #include "macros.h"
 
 // external global variables
 extern MainWindow *qmc2MainWindow;
-extern Options *qmc2Options;
+extern QSettings *qmc2Config;
 
 DocBrowser::DocBrowser(QWidget *parent)
   : QDialog(parent)
@@ -19,9 +20,11 @@ DocBrowser::DocBrowser(QWidget *parent)
   browser = new MiniWebBrowser(this);
   verticalLayout->addWidget(browser);
 
-  widgetSize = QSize(-1, -1);
-  widgetPosValid = FALSE;
-  ignoreResizeAndMove = TRUE;
+  widgetSize = qmc2Config->value(QMC2_FRONTEND_PREFIX + "Layout/DocBrowser/Size", QSize(600, 600)).toSize();
+  resize(widgetSize);
+
+  widgetPos = qmc2Config->value(QMC2_FRONTEND_PREFIX + "Layout/DocBrowser/Pos", QPoint((parent->width() - width()) / 2, (parent->height() - height()) / 2)).toPoint();
+  move(widgetPos);
 }
 
 DocBrowser::~DocBrowser()
@@ -30,6 +33,8 @@ DocBrowser::~DocBrowser()
   qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DocBrowser::~DocBrowser()");
 #endif
 
+  qmc2Config->setValue(QMC2_FRONTEND_PREFIX + "Layout/DocBrowser/Size", size());
+  qmc2Config->setValue(QMC2_FRONTEND_PREFIX + "Layout/DocBrowser/Pos", pos());
 }
 
 void DocBrowser::showEvent(QShowEvent *e)
@@ -37,15 +42,6 @@ void DocBrowser::showEvent(QShowEvent *e)
 #ifdef QMC2_DEBUG
   qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DocBrowser::showEvent(QShowEvent *e = 0x" + QString::number((ulong)e, 16) + ")");
 #endif
-
-  ignoreResizeAndMove = TRUE;
-
-  if ( widgetSize.isValid() )
-    resize(widgetSize);
-  if ( widgetPosValid )
-    move(widgetPos);
-
-  ignoreResizeAndMove = FALSE;
 
   e->accept();
 }
@@ -56,9 +52,6 @@ void DocBrowser::resizeEvent(QResizeEvent *e)
   qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DocBrowser::resizeEvent(QResizeEvent *e = 0x" + QString::number((ulong)e, 16) + ")");
 #endif
 
-  if ( !ignoreResizeAndMove )
-    widgetSize = size();
-
   e->accept();
 }
 
@@ -67,11 +60,6 @@ void DocBrowser::moveEvent(QMoveEvent *e)
 #ifdef QMC2_DEBUG
   qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DocBrowser::moveEvent(QMoveEvent *e = 0x" + QString::number((ulong)e, 16) + ")");
 #endif
-
-  if ( !ignoreResizeAndMove ) {
-    widgetPos = pos();
-    widgetPosValid = TRUE;
-  }
 
   e->accept();
 }
