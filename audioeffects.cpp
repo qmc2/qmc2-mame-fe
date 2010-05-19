593fe9f13c25e717ef335a37c74ff5ce35b8b2dd
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2010-05-19 16:04:41 +0000

    wip: added framework for the audio effects dialog

---
+++ audioeffects.cpp
@@ -0,0 +1,28 @@
+#if QMC2_USE_PHONON_API == 1
+
+#include "audioeffects.h"
+#include "qmc2main.h"
+
+extern MainWindow *qmc2MainWindow;
+
+#define QMC2_DEBUG
+
+AudioEffectDialog::AudioEffectDialog(QWidget *parent)
+  : QDialog(parent)
+{
+#ifdef QMC2_DEBUG
+	qmc2MainWindow->log(QMC2_LOG_FRONTEND, QString("DEBUG: AudioEffectDialog::AudioEffectDialog(QWidget *parent = %1)").arg(filePath).arg(filter).arg((qulonglong) parent));
+#endif
+
+	setupUi(this);
+}
+
+AudioEffectDialog::~AudioEffectDialog()
+{
+#ifdef QMC2_DEBUG
+	qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: AudioEffectDialog::~AudioEffectDialog()");
+#endif
+
+}
+
+#endif
