e277b019f3299fa32aa2e7a8792de8e5f1ebfe8d
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2011-06-21 03:22:50 +0000

    imp: added a separating "," between the float values

--- trunk/floateditwidget.cpp
+++ trunk/floateditwidget.cpp
@@ -6,22 +6,28 @@
 extern MainWindow *qmc2MainWindow;
 #endif
 
-FloatEditWidget::FloatEditWidget(int nValues, QWidget *parent)
+FloatEditWidget::FloatEditWidget(int nValues, QString sep, QWidget *parent)
 	: QWidget(parent)
 {
 #ifdef QMC2_DEBUG
-	qmc2MainWindow->log(QMC2_LOG_FRONTEND, QString("DEBUG: FloatEditWidget::FloatEditWidget(int nValues = %1, QWidget *parent = %2)").arg(nValues).arg((qulonglong) parent));
+	qmc2MainWindow->log(QMC2_LOG_FRONTEND, QString("DEBUG: FloatEditWidget::FloatEditWidget(int nValues = %1, QString sep = %2, QWidget *parent = %3)").arg(nValues).arg(sep).arg((qulonglong) parent));
 #endif
 
 	setupUi(this);
 
 	numValues = nValues;
 
-	if ( numValues < 3 )
+	if ( numValues < 3 ) {
 		doubleSpinBox2->hide();
+		labelSeparator1->hide();
+	} else
+		labelSeparator1->setText(sep);
 
-	if ( numValues < 2 )
+	if ( numValues < 2 ) {
 		doubleSpinBox1->hide();
+		labelSeparator0->hide();
+	} else
+		labelSeparator0->setText(sep);
 }
 
 FloatEditWidget::~FloatEditWidget()
