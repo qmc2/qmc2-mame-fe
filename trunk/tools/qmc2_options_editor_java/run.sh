00ca2130f369960ae74e74032625a4b22e4a7429
mmbossoni <mmbossoni@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2011-07-03 00:50:36 +0000

    Added build/run scripts

---
+++ trunk/tools/qmc2_options_editor_java/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp org.eclipse.core.commands_3.6.0.I20110111-0800.jar:org.eclipse.equinox.common_3.6.0.v20110523.jar:org.eclipse.jface_3.7.0.I20110522-1430.jar:org.eclipse.osgi_3.7.0.v20110613.jar:org.eclipse.swt.gtk.linux.x86_64_3.7.0.v3735b.jar:bin sourceforge.org.qmc2.options.editor.QMC2EditorApplication
