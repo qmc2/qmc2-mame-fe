68fecc23458f5c785d4c0181daa7a2f6d252211d
mmbossoni <mmbossoni@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2011-07-13 00:23:16 +0000

    Add operation concept in editor
    Implement first operation (edit operation)
    
    Add operation stack (support for undo/redo of everything implemented using operations)
    
    Add hotkeys for undo/redo

--- trunk/tools/qmc2_options_editor_java/src/sourceforge/org/qmc2/options/editor/ui/QMC2EditingSupport.java
+++ trunk/tools/qmc2_options_editor_java/src/sourceforge/org/qmc2/options/editor/ui/QMC2EditingSupport.java
@@ -1,23 +1,28 @@
 package sourceforge.org.qmc2.options.editor.ui;
 
 import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.EditingSupport;
 import org.eclipse.jface.viewers.TextCellEditor;
 import org.eclipse.swt.widgets.Composite;
 
 import sourceforge.org.qmc2.options.editor.model.DescriptableItem;
+import sourceforge.org.qmc2.options.editor.ui.operations.EditOperation;
+import sourceforge.org.qmc2.options.editor.ui.operations.IOperation;
 
 public class QMC2EditingSupport extends EditingSupport {
 
 	private final String lang;
 
-	private CellEditor editor;
+	private final CellEditor editor;
 
-	public QMC2EditingSupport(ColumnViewer viewer, String language) {
-		super(viewer);
+	private final QMC2Editor qmc2Editor;
+
+	public QMC2EditingSupport(QMC2Editor editor, String language) {
+		super(editor.getViewer());
 		this.lang = language;
-		this.editor = new TextCellEditor((Composite) viewer.getControl());
+		this.qmc2Editor = editor;
+		this.editor = new TextCellEditor((Composite) editor.getViewer()
+				.getControl());
 
 	}
 
@@ -44,8 +49,9 @@ public class QMC2EditingSupport extends EditingSupport {
 	@Override
 	protected void setValue(Object item, Object value) {
 		if (item instanceof DescriptableItem) {
-			((DescriptableItem) item).setDescription(lang, value.toString());
-			getViewer().refresh(item);
+			IOperation operation = new EditOperation(getViewer(),
+					(DescriptableItem) item, lang, value.toString());
+			qmc2Editor.getOperationStack().execute(operation);
 		}
 
 	}
