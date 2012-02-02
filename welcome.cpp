1d54859deb969b39d78f9436d779d310297e389e
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-02-02 09:35:18 +0000

    imp: adopted new logos to the welcome dialog

--- welcome.cpp
+++ welcome.cpp
@@ -30,7 +30,7 @@ Welcome::Welcome(QWidget *parent)
     setupUi(this);
 
 #if defined(QMC2_EMUTYPE_MESS)
-    labelLogoPixmap->setPixmap(QString::fromUtf8(":/data/img/qmc2_mess_logo_small.png"));
+    labelLogoPixmap->setPixmap(QString::fromUtf8(":/data/img/qmc2_mess_logo_big.png"));
 #endif
 
 #if defined(QMC2_SDLMAME)
