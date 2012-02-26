a802c287f2611c04215f6fdd904c405d5e53bd6d
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-02-26 14:37:45 +0000

    imp: build: cleaned up a number of DEBUG messages (DEBUG=2 builds)

--- trunk/direditwidget.cpp
+++ trunk/direditwidget.cpp
@@ -10,24 +10,24 @@ extern MainWindow *qmc2MainWindow;
 extern QString qmc2DirectoryEditStartPath;
 
 DirectoryEditWidget::DirectoryEditWidget(QString dirPath, QWidget *parent)
-  : QWidget(parent)
+	: QWidget(parent)
 {
 #ifdef QMC2_DEBUG
-  qmc2MainWindow->log(QMC2_LOG_FRONTEND, QString("DEBUG: DirectoryEditWidget::DirectoryEditWidget(QString filePath = %1, QWidget *parent = %2").arg(dirPath).arg((qulonglong) parent));
+	qmc2MainWindow->log(QMC2_LOG_FRONTEND, QString("DEBUG: DirectoryEditWidget::DirectoryEditWidget(QString filePath = %1, QWidget *parent = %2)").arg(dirPath).arg((qulonglong) parent));
 #endif
 
-  setupUi(this);
+	setupUi(this);
 
-  lineEditDirectory->setText(dirPath);
-  QFontMetrics fm(QApplication::font());
-  QSize iconSize(fm.height() - 2, fm.height() - 2);
-  toolButtonBrowse->setIconSize(iconSize);
+	lineEditDirectory->setText(dirPath);
+	QFontMetrics fm(QApplication::font());
+	QSize iconSize(fm.height() - 2, fm.height() - 2);
+	toolButtonBrowse->setIconSize(iconSize);
 }
 
 DirectoryEditWidget::~DirectoryEditWidget()
 {
 #ifdef QMC2_DEBUG
-  qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DirectoryEditWidget::~DirectoryEditWidget()");
+	qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DirectoryEditWidget::~DirectoryEditWidget()");
 #endif
 
 }
@@ -35,13 +35,13 @@ DirectoryEditWidget::~DirectoryEditWidget()
 void DirectoryEditWidget::on_toolButtonBrowse_clicked()
 {
 #ifdef QMC2_DEBUG
-  qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DirectoryEditWidget::on_toolButtonBrowse_clicked()");
+	qmc2MainWindow->log(QMC2_LOG_FRONTEND, "DEBUG: DirectoryEditWidget::on_toolButtonBrowse_clicked()");
 #endif
 
-  QString startPath = lineEditDirectory->text();
-  if ( startPath.isEmpty() )
-    startPath = qmc2DirectoryEditStartPath;
-  QString s = QFileDialog::getExistingDirectory(this, tr("Choose directory"), startPath);
-  if ( !s.isEmpty() )
-    lineEditDirectory->setText(s);
+	QString startPath = lineEditDirectory->text();
+	if ( startPath.isEmpty() )
+		startPath = qmc2DirectoryEditStartPath;
+	QString s = QFileDialog::getExistingDirectory(this, tr("Choose directory"), startPath);
+	if ( !s.isEmpty() )
+		lineEditDirectory->setText(s);
 }
