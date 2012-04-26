4bec853f3eca6747e08ea7cdd92dd228e8901716
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-04-26 16:14:08 +0000

    imp: added Windows (MinGW) specific "uname -r" replacement

---
+++ trunk/arch/Windows/osrel.bat
@@ -0,0 +1,7 @@
+@Echo off
+Setlocal
+:: Get windows Version numbers
+For /f "tokens=2 delims=[]" %%G in ('ver') Do (set _version=%%G)
+For /f "tokens=2,3,4 delims=. " %%G in ('echo %_version%') Do (set _major=%%G& set _minor=%%H& set _build=%%I)
+set WINDOWS_VERSION=%_major%.%_minor%.%_build%
+echo %WINDOWS_VERSION%
