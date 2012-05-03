4d7beaa39a8a351c08a08ec5c9e3edc6ea97205a
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-05-03 17:47:25 +0000

    wip: added preferences dialog framework and more

--- trunk/tools/qchdman/projectwidget.cpp
+++ trunk/tools/qchdman/projectwidget.cpp
@@ -1,4 +1,5 @@
 #include <QtGui>
+#include <QFileDialog>
 
 #include "projectwidget.h"
 #include "ui_projectwidget.h"
@@ -47,6 +48,20 @@ void ProjectWidget::on_toolButtonRun_clicked()
 void ProjectWidget::on_splitter_splitterMoved(int, int)
 {
     globalConfig->setProjectWidgetSplitterSizes(splitterSizes());
+}
+
+void ProjectWidget::on_toolButtonBrowseVerifyInputFile_clicked()
+{
+    QString s = QFileDialog::getOpenFileName(this, tr("Choose CHD input file"), ui->lineEditVerifyInputFile->text(), tr("CHD files (*.chd);;All files (*)"));
+    if ( !s.isNull() )
+        ui->lineEditVerifyInputFile->setText(s);
+}
+
+void ProjectWidget::on_toolButtonBrowseVerifyParentInputFile_clicked()
+{
+    QString s = QFileDialog::getOpenFileName(this, tr("Choose parent CHD input file"), ui->lineEditVerifyParentInputFile->text(), tr("CHD files (*.chd);;All files (*)"));
+    if ( !s.isNull() )
+        ui->lineEditVerifyParentInputFile->setText(s);
 }
 
 void ProjectWidget::log(QString message)
--- trunk/tools/qchdman/projectwidget.h
+++ trunk/tools/qchdman/projectwidget.h
@@ -21,6 +21,11 @@ public slots:
     void on_comboBoxProjectType_currentIndexChanged(int);
     void on_toolButtonRun_clicked();
     void on_splitter_splitterMoved(int, int);
+
+    // Verify
+    void on_toolButtonBrowseVerifyInputFile_clicked();
+    void on_toolButtonBrowseVerifyParentInputFile_clicked();
+
     void log(QString);
 
 private:
