4fb1379ad4dc30784dbcbe87f6093d467bf899bf
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-06-11 07:32:32 +0000

    fix: use different file-dialogs depending on the context

--- trunk/fileeditwidget.cpp
+++ trunk/fileeditwidget.cpp
@@ -47,7 +47,11 @@ void FileEditWidget::on_toolButtonBrowse_clicked()
 	QString startPath = lineEditFile->text();
 	if ( startPath.isEmpty() )
 		startPath = qmc2FileEditStartPath;
-	QString s = QFileDialog::getSaveFileName(this, tr("Choose file"), startPath, browserFilter);
+	QString s;
+	if ( toolButtonClear->isVisible() )
+		s = QFileDialog::getOpenFileName(this, tr("Choose file"), startPath, browserFilter);
+	else
+		s = QFileDialog::getSaveFileName(this, tr("Choose file"), startPath, browserFilter);
 	if ( !s.isEmpty() ) {
 		if ( browserPart == "basename" ) {
 			QFileInfo fi(s);
