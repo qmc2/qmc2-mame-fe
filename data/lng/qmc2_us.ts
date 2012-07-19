879f42747bea5f80dca45de09b60f317cd607c33
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-07-19 08:23:27 +0000

    imp: minor Windows-specific text/translation update

--- data/lng/qmc2_us.ts
+++ data/lng/qmc2_us.ts
@@ -2089,7 +2089,12 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>DirectoryModel</name>
     <message>
-        <location filename="../../filesystemmodel.h" line="36"/>
+        <location filename="../../filesystemmodel.h" line="37"/>
+        <source>Drives / Folders</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../filesystemmodel.h" line="39"/>
         <source>Folders</source>
         <translation></translation>
     </message>
@@ -2615,40 +2620,40 @@ p, li { white-space: pre-wrap; }
 <context>
     <name>FileSystemModel</name>
     <message>
-        <location filename="../../filesystemmodel.h" line="296"/>
+        <location filename="../../filesystemmodel.h" line="300"/>
         <source>Name</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../filesystemmodel.h" line="296"/>
+        <location filename="../../filesystemmodel.h" line="300"/>
         <source>Size</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../filesystemmodel.h" line="486"/>
-        <location filename="../../filesystemmodel.h" line="500"/>
+        <location filename="../../filesystemmodel.h" line="490"/>
+        <location filename="../../filesystemmodel.h" line="504"/>
         <source> KB</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../filesystemmodel.h" line="489"/>
-        <location filename="../../filesystemmodel.h" line="503"/>
+        <location filename="../../filesystemmodel.h" line="493"/>
+        <location filename="../../filesystemmodel.h" line="507"/>
         <source> MB</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../filesystemmodel.h" line="492"/>
-        <location filename="../../filesystemmodel.h" line="506"/>
+        <location filename="../../filesystemmodel.h" line="496"/>
+        <location filename="../../filesystemmodel.h" line="510"/>
         <source> GB</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../filesystemmodel.h" line="495"/>
+        <location filename="../../filesystemmodel.h" line="499"/>
         <source> TB</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../../filesystemmodel.h" line="296"/>
+        <location filename="../../filesystemmodel.h" line="300"/>
         <source>Date modified</source>
         <translation></translation>
     </message>
