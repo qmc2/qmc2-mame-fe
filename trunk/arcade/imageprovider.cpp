eaf40b86406fa809033293fb1a49ed5420c7244c
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2012-12-17 12:19:51 +0000

    imp: arcade mode: optimized & completed the image provider

--- trunk/arcade/imageprovider.cpp
+++ trunk/arcade/imageprovider.cpp
@@ -37,93 +37,22 @@ QImage ImageProvider::requestImage(const QString &id, QSize *size, const QSize &
         QString imageType = idWords[0];
         QString gameId = idWords[1];
         QString fileName;
-        if ( imageType == "prv" ) {
-            if ( globalConfig->previewsZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->previewFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
-        } else if ( imageType == "fly" ) {
-            if ( globalConfig->flyersZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->flyerFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
-        } else if ( imageType == "cab" ) {
-            if ( globalConfig->cabinetsZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->cabinetFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
-        } else if ( imageType == "ctl" ) {
-            if ( globalConfig->controllersZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->controllerFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
-        } else if ( imageType == "mrq" ) {
-            if ( globalConfig->marqueesZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->marqueeFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
-        } else if ( imageType == "ttl" ) {
-            if ( globalConfig->titlesZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->titleFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
-        } else if ( imageType == "pcb" ) {
-            if ( globalConfig->pcbsZipped() )
-                return loadZippedImage(imageType, gameId);
-            else {
-                foreach (QString folder, globalConfig->pcbFolder().split(";", QString::SkipEmptyParts)) {
-                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
-                    if ( !image.load(fileName) )
-                        image.load(QLatin1String(":/images/ghost.png"));
-                    else
-                        break;
-                }
-            }
+        if ( isZippedImageType(imageType) ) {
+            return loadZippedImage(imageType, gameId);
         } else {
-            QMC2_ARCADE_LOG_STR(QObject::tr("WARNING: ImageProvider::requestImage(): invalid image type '%1' requested").arg(imageType));
-            image.load(QLatin1String(":/images/ghost.png"));
+            QStringList imageFolders = imageFolder(imageType).split(";", QString::SkipEmptyParts);
+            if ( !imageFolders.isEmpty() ) {
+                foreach (QString folder, imageFolders) {
+                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
+                    if ( !image.load(fileName) )
+                        image.load(QLatin1String(":/images/ghost.png"));
+                    else
+                        break;
+                }
+            } else {
+                QMC2_ARCADE_LOG_STR(QObject::tr("WARNING: ImageProvider::requestImage(): invalid image type '%1' requested").arg(imageType));
+                image.load(QLatin1String(":/images/ghost.png"));
+            }
         }
     } else {
         QMC2_ARCADE_LOG_STR(QObject::tr("WARNING: ImageProvider::requestImage(): invalid image ID '%1' requested").arg(id));
@@ -138,10 +67,41 @@ QImage ImageProvider::requestImage(const QString &id, QSize *size, const QSize &
     return result;
 }
 
-QPixmap ImageProvider::requestPixmap(const QString &/*id*/, QSize */*size*/, const QSize &/*requestedSize*/)
+QPixmap ImageProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    // FIXME (however, this isn't used anyway)
-    QPixmap result;
+    QPixmap image, result;
+    QStringList idWords = id.split("/", QString::SkipEmptyParts);
+    if ( idWords.count() > 1 ) {
+        QString imageType = idWords[0];
+        QString gameId = idWords[1];
+        QString fileName;
+        if ( isZippedImageType(imageType) ) {
+            return loadZippedPixmap(imageType, gameId);
+        } else {
+            QStringList imageFolders = imageFolder(imageType).split(";", QString::SkipEmptyParts);
+            if ( !imageFolders.isEmpty() ) {
+                foreach (QString folder, imageFolders) {
+                    fileName = QFileInfo(folder + "/" + gameId + ".png").absoluteFilePath();
+                    if ( !image.load(fileName) )
+                        image.load(QLatin1String(":/images/ghost.png"));
+                    else
+                        break;
+                }
+            } else {
+                QMC2_ARCADE_LOG_STR(QObject::tr("WARNING: ImageProvider::requestImage(): invalid image type '%1' requested").arg(imageType));
+                image.load(QLatin1String(":/images/ghost.png"));
+            }
+        }
+    } else {
+        QMC2_ARCADE_LOG_STR(QObject::tr("WARNING: ImageProvider::requestImage(): invalid image ID '%1' requested").arg(id));
+        image.load(QLatin1String(":/images/ghost.png"));
+    }
+    if ( requestedSize.isValid() )
+        result = image.scaled(requestedSize, Qt::KeepAspectRatio);
+    else
+        result = image;
+    if ( size )
+        *size = result.size();
     return result;
 }
 
@@ -167,62 +127,112 @@ QImage ImageProvider::loadZippedImage(QString imageType, QString id)
     return zippedImage;
 }
 
+QPixmap ImageProvider::loadZippedPixmap(QString imageType, QString id)
+{
+    QPixmap zippedImage;
+    QString imageFileName = id + ".png";
+    unzFile imageFile = mZipFileMap[imageType];
+    if ( imageFile && unzLocateFile(imageFile, (const char *)imageFileName.toLocal8Bit(), 0) == UNZ_OK ) {
+        QByteArray imageData;
+        char imageBuffer[QMC2_ARCADE_ZIP_BUFSIZE];
+        if ( unzOpenCurrentFile(imageFile) == UNZ_OK ) {
+            int len = 0;
+            while ( (len = unzReadCurrentFile(imageFile, &imageBuffer, QMC2_ARCADE_ZIP_BUFSIZE)) > 0 ) {
+                for (int i = 0; i < len; i++)
+                    imageData += imageBuffer[i];
+            }
+            unzCloseCurrentFile(imageFile);
+        }
+        zippedImage.loadFromData(imageData, "PNG");
+    } else
+        zippedImage.load(QLatin1String(":/images/ghost.png"));
+    return zippedImage;
+}
+
 QString ImageProvider::imageTypeToZipFile(QString type)
 {
-    if ( type == "prv" )
+    switch ( mImageTypes.indexOf(type) ) {
+    case 0:
         return globalConfig->previewZipFile();
-    else if ( type == "fly" )
+    case 1:
         return globalConfig->flyerZipFile();
-    else if ( type == "cab" )
+    case 2:
         return globalConfig->cabinetZipFile();
-    else if ( type == "ctl" )
+    case 3:
         return globalConfig->controllerZipFile();
-    else if ( type == "mrq" )
+    case 4:
         return globalConfig->marqueeZipFile();
-    else if ( type == "ttl" )
+    case 5:
         return globalConfig->titleZipFile();
-    else if ( type == "pcb" )
+    case 6:
         return globalConfig->pcbZipFile();
-    else
+    default:
         return QString();
+    }
 }
 
 QString ImageProvider::imageTypeToLongName(QString type)
 {
-    if ( type == "prv" )
+    switch ( mImageTypes.indexOf(type) ) {
+    case 0:
         return QObject::tr("preview");
-    else if ( type == "fly" )
+    case 1:
         return QObject::tr("flyer");
-    else if ( type == "cab" )
+    case 2:
         return QObject::tr("cabinet");
-    else if ( type == "ctl" )
+    case 3:
         return QObject::tr("controller");
-    else if ( type == "mrq" )
+    case 4:
         return QObject::tr("marquee");
-    else if ( type == "ttl" )
+    case 5:
         return QObject::tr("title");
-    else if ( type == "pcb" )
+    case 6:
         return QObject::tr("PCB");
-    else
+    default:
         return QString();
+    }
 }
 
 bool ImageProvider::isZippedImageType(QString type)
 {
-    if ( type == "prv" )
+    switch ( mImageTypes.indexOf(type) ) {
+    case 0:
         return globalConfig->previewsZipped();
-    else if ( type == "fly" )
+    case 1:
         return globalConfig->flyersZipped();
-    else if ( type == "cab" )
+    case 2:
         return globalConfig->cabinetsZipped();
-    else if ( type == "ctl" )
+    case 3:
         return globalConfig->controllersZipped();
-    else if ( type == "mrq" )
+    case 4:
         return globalConfig->marqueesZipped();
-    else if ( type == "ttl" )
+    case 5:
         return globalConfig->titlesZipped();
-    else if ( type == "pcb" )
+    case 6:
         return globalConfig->pcbsZipped();
-    else
+    default:
         return false;
+    }
+}
+
+QString ImageProvider::imageFolder(QString type)
+{
+    switch ( mImageTypes.indexOf(type) ) {
+    case 0:
+        return globalConfig->previewFolder();
+    case 1:
+        return globalConfig->flyerFolder();
+    case 2:
+        return globalConfig->cabinetFolder();
+    case 3:
+        return globalConfig->controllerFolder();
+    case 4:
+        return globalConfig->marqueeFolder();
+    case 5:
+        return globalConfig->titleFolder();
+    case 6:
+        return globalConfig->pcbFolder();
+    default:
+        return QString();
+    }
 }
