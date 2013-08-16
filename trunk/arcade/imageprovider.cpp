440cf172516c563919eb51e816cd193883c1548a
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2013-08-16 09:22:23 +0000

    wip: updated image format extensions to use same list-format as main GUI

--- trunk/arcade/imageprovider.cpp
+++ trunk/arcade/imageprovider.cpp
@@ -31,9 +31,9 @@ ImageProvider::ImageProvider(QQuickImageProvider::ImageType type)
         if ( activeFormats.isEmpty() )
             mActiveFormatsMap[imageType] << QMC2_ARCADE_IMAGE_FORMAT_INDEX_PNG;
         else for (int i = 0; i < activeFormats.count(); i++)
-            mActiveFormatsMap[imageType]<< activeFormats[i].toInt();
+            mActiveFormatsMap[imageType] << activeFormats[i].toInt();
     }
-    mFormatExtensions << "png" << "bmp" << "gif" << "jpg:jpeg" << "pbm" << "pgm" << "ppm" << "tif:tiff" << "xbm" << "xpm" << "svg" << "tga";
+    mFormatExtensions << "png" << "bmp" << "gif" << "jpg, jpeg" << "pbm" << "pgm" << "ppm" << "tif, tiff" << "xbm" << "xpm" << "svg" << "tga";
 }
 
 ImageProvider::~ImageProvider()
