acbb0fce59dac117a2eb82a878440b5876e1514c
elbeardmorez <elbeardmorez@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2013-11-14 20:58:13 +0000

    add: [darkone] joystick key mappings

--- trunk/arcade/keysequences.h
+++ trunk/arcade/keysequences.h
@@ -1,7 +1,21 @@
 #ifndef KEYSEQUENCES_H
 #define KEYSEQUENCES_H
 
-#define QMC2_ARCADE_ADD_COMMON_KEYSEQUENCES(stringList)         (stringList) << "Up" \
+// common key-sequences
+#define QMC2_ARCADE_ADD_COMMON_KEYSEQUENCES(stringList)         (stringList) << "Enter" \
+                                                                             << "Return" \
+                                                                             << "F11" \
+                                                                             << "Alt+Enter" \
+                                                                             << "Alt+Return"
+
+// common key-sequence descriptions
+#define QMC2_ARCADE_ADD_COMMON_DESCRIPTIONS(stringList)         (stringList) << QObject::tr("Start emulation") \
+                                                                             << QObject::tr("Start emulation") \
+                                                                             << QObject::tr("Toggle full-screen / windowed mode") \
+                                                                             << QObject::tr("Toggle full-screen / windowed mode") \
+                                                                             << QObject::tr("Toggle full-screen / windowed mode")
+// toxicwaste-specific key-sequences
+#define QMC2_ARCADE_ADD_TOXIXCWASTE_KEYSEQUENCES(stringList)    (stringList) << "Up" \
                                                                              << "Down" \
                                                                              << "Left" \
                                                                              << "Right" \
@@ -9,10 +23,15 @@
                                                                              << "PgDown" \
                                                                              << "Home" \
                                                                              << "End" \
+                                                                             << "Ctrl+F" \
+                                                                             << "Ctrl+M" \
+                                                                             << "Ctrl+O" \
+                                                                             << "Ctrl+P" \
                                                                              << "Esc" \
-                                                                             << "Enter" \
-                                                                             << "Return"
-#define QMC2_ARCADE_ADD_COMMON_DESCRIPTIONS(stringList)         (stringList) << QObject::tr("Cursor up") \
+                                                                             << "Ctrl+X" \
+                                                                             << "Ctrl+Backspace"
+// toxicwaste-specific key-sequence dscriptions
+#define QMC2_ARCADE_ADD_TOXIXCWASTE_DESCRIPTIONS(stringList)    (stringList) << QObject::tr("Cursor up") \
                                                                              << QObject::tr("Cursor down") \
                                                                              << QObject::tr("Cursor left") \
                                                                              << QObject::tr("Cursor right") \
@@ -20,28 +39,70 @@
                                                                              << QObject::tr("Page down") \
                                                                              << QObject::tr("Start of list") \
                                                                              << QObject::tr("End of list") \
-                                                                             << QObject::tr("Exit") \
-                                                                             << QObject::tr("Start emulation") \
-                                                                             << QObject::tr("Start emulation")
-#define QMC2_ARCADE_ADD_TOXIXCWASTE_KEYSEQUENCES(stringList)    (stringList) << "F11" \
-                                                                             << "Alt+Enter" \
-                                                                             << "Alt+Return" \
-                                                                             << "Ctrl+F" \
-                                                                             << "Ctrl+M" \
-                                                                             << "Ctrl+O" \
-                                                                             << "Ctrl+P" \
-                                                                             << "Ctrl+X" \
-                                                                             << "Ctrl+Backspace"
-#define QMC2_ARCADE_ADD_TOXIXCWASTE_DESCRIPTIONS(stringList)    (stringList) << QObject::tr("Toggle full-screen / windowed mode") \
-                                                                             << QObject::tr("Toggle full-screen / windowed mode") \
-                                                                             << QObject::tr("Toggle full-screen / windowed mode") \
                                                                              << QObject::tr("Focus search box") \
                                                                              << QObject::tr("Toggle menu-bar") \
                                                                              << QObject::tr("Toggle preferences") \
                                                                              << QObject::tr("Start emulation") \
                                                                              << QObject::tr("Exit") \
+                                                                             << QObject::tr("Exit") \
                                                                              << QObject::tr("Flip cabinet / game-card")
-#define QMC2_ARCADE_ADD_DARKONE_KEYSEQUENCES(stringList)        ; // FIXME: add darkone-specific key-sequences
-#define QMC2_ARCADE_ADD_DARKONE_DESCRIPTIONS(stringList)        ; // FIXME: add darkone-specific key-sequence descriptions
+// darkone-specific key-sequences
+#define QMC2_ARCADE_ADD_DARKONE_KEYSEQUENCES(stringList)        (stringList) << "Space" \
+                                                                             << "Up" \
+                                                                             << "PgUp" \
+                                                                             << "Ctrl+Up" \
+                                                                             << "Home" \
+                                                                             << "Ctrl+Shift+Up" \
+                                                                             << "Down" \
+                                                                             << "PgDown" \
+                                                                             << "Ctrl+Down" \
+                                                                             << "End" \
+                                                                             << "Ctrl+Shift+Down" \
+                                                                             << "Left" \
+                                                                             << "Right" \
+                                                                             << "Tab" \
+                                                                             << "Ctrl+Down" \
+                                                                             << "Ctrl+Right" \
+                                                                             << "Shift+Tab" \
+                                                                             << "Ctrl+Up" \
+                                                                             << "Ctrl+Left" \
+                                                                             << "Ctrl+Shift+Up" \
+                                                                             << "Ctrl+Shift+Down" \
+                                                                             << "Ctrl+P" \
+                                                                             << "Ctrl+O" \
+                                                                             << "Ctrl+T" \
+                                                                             << "Ctrl+S" \
+                                                                             << "Alt+F" \
+                                                                             << "Esc" \
+                                                                             << "Ctrl+Q"
+// darkone-specific key-sequence descriptions
+ #define QMC2_ARCADE_ADD_DARKONE_DESCRIPTIONS(stringList)       (stringList) << QObject::tr("Select") \
+                                                                             << QObject::tr("List up") \
+                                                                             << QObject::tr("List page up") \
+                                                                             << QObject::tr("List page up") \
+                                                                             << QObject::tr("List top") \
+                                                                             << QObject::tr("List top") \
+                                                                             << QObject::tr("List down") \
+                                                                             << QObject::tr("List page down") \
+                                                                             << QObject::tr("List page down") \
+                                                                             << QObject::tr("List bottom") \
+                                                                             << QObject::tr("List bottom") \
+                                                                             << QObject::tr("Hide list") \
+                                                                             << QObject::tr("Show list") \
+                                                                             << QObject::tr("Next widget") \
+                                                                             << QObject::tr("Next widget") \
+                                                                             << QObject::tr("Next widget") \
+                                                                             << QObject::tr("Previous widget") \
+                                                                             << QObject::tr("Previous widget") \
+                                                                             << QObject::tr("Previous widget") \
+                                                                             << QObject::tr("Zoom in") \
+                                                                             << QObject::tr("Zoom out") \
+                                                                             << QObject::tr("Start emulation") \
+                                                                             << QObject::tr("Toggle options") \
+                                                                             << QObject::tr("Toggle toolbar") \
+                                                                             << QObject::tr("Search") \
+                                                                             << QObject::tr("Toggle full-screen / windowed mode") \
+                                                                             << QObject::tr("Abort game launch") \
+                                                                             << QObject::tr("Exit")
 
 #endif // KEYSEQUENCES_H
