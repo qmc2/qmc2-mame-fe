0fe16f5eb480f38cfce77c2ef17f0c5698087f80
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2013-11-19 06:32:10 +0000

    fix: [darkone] added missing qsTr()'s

--- arcade/translations/qmc2-arcade_de.ts
+++ arcade/translations/qmc2-arcade_de.ts
@@ -1186,13 +1186,17 @@ QMC2 Arcade neu starten!</translation>
         <translation>Farb Schema</translation>
     </message>
     <message>
+        <location filename="../qml/darkone/1.1/darkone.js" line="513"/>
         <location filename="../qml/darkone/1.1/darkone.qml" line="1412"/>
+        <location filename="../qml/darkone/2.0/darkone.js" line="513"/>
         <location filename="../qml/darkone/2.0/darkone.qml" line="1386"/>
         <source>dark</source>
         <translation>dunkel</translation>
     </message>
     <message>
+        <location filename="../qml/darkone/1.1/darkone.js" line="524"/>
         <location filename="../qml/darkone/1.1/darkone.qml" line="1434"/>
+        <location filename="../qml/darkone/2.0/darkone.js" line="524"/>
         <location filename="../qml/darkone/2.0/darkone.qml" line="1408"/>
         <source>metal</source>
         <translation>metallisch</translation>
--- arcade/translations/qmc2-arcade_es.ts
+++ arcade/translations/qmc2-arcade_es.ts
@@ -1184,13 +1184,17 @@ take effect, please restart QMC2 Arcade!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/darkone/1.1/darkone.js" line="513"/>
         <location filename="../qml/darkone/1.1/darkone.qml" line="1412"/>
+        <location filename="../qml/darkone/2.0/darkone.js" line="513"/>
         <location filename="../qml/darkone/2.0/darkone.qml" line="1386"/>
         <source>dark</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../qml/darkone/1.1/darkone.js" line="524"/>
         <location filename="../qml/darkone/1.1/darkone.qml" line="1434"/>
+        <location filename="../qml/darkone/2.0/darkone.js" line="524"/>
         <location filename="../qml/darkone/2.0/darkone.qml" line="1408"/>
         <source>metal</source>
         <translation type="unfinished"></translation>
