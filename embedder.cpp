7a83cc42b44a5fae419440b1e68e3be17ae0ac29
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2013-11-27 21:17:15 +0000

    imp: code clean up

--- embedder.cpp
+++ embedder.cpp
@@ -181,7 +181,6 @@ void Embedder::release()
 	qApp->syncX();
 	XUnmapWindow(QX11Info::display(), embeddedWinId);
 	XMapWindow(QX11Info::display(), embeddedWinId);
-	XMapSubwindows(QX11Info::display(), embeddedWinId);
 	qmc2MainWindow->log(QMC2_LOG_FRONTEND, tr("emulator #%1 released, window ID = %2").arg(gameID).arg("0x" + QString::number(embeddedWinId, 16)));
 	QTimer::singleShot(QMC2_EMBED_RELEASE_DELAY, qmc2MainWindow, SLOT(raise()));
 #elif defined(QMC2_OS_WIN)
