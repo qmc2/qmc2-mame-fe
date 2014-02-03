da2c4685f34415e111ed1cfe5a2ec61d24dd2ee7
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2014-02-03 14:49:54 +0000

    wip: XML cache database updates #2

--- trunk/xmldbmgr.h
+++ trunk/xmldbmgr.h
@@ -12,11 +12,21 @@ class XmlDatabaseManager : public QObject
 		explicit XmlDatabaseManager(QObject *parent);
 		~XmlDatabaseManager();
 
+		QString emulatorVersion();
+		void setEmulatorVersion(QString);
+		QString qmc2Version();
+		void setQmc2Version(QString);
+		int xmlCacheVersion();
+		void setXmlCacheVersion(int);
+		QString dtd();
+		void setDtd(QString);
+
 	public slots:
 		void recreateDatabase();
 
 	private:
 		mutable QSqlDatabase m_db;
+		QString m_tableBasename;
 };
 
 #endif
