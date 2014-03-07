995f54f056040e3983c0ad18ffa5b4bda584009d
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2014-03-07 18:19:52 +0000

    imp: removed some unsused files from PDF.js

--- data/js/pdfjs/web/viewer.js
+++ data/js/pdfjs/web/viewer.js
@@ -23,7 +23,7 @@
 
 'use strict';
 
-var DEFAULT_URL = 'compressed.tracemonkey-pldi-09.pdf';
+var DEFAULT_URL = '';
 var DEFAULT_SCALE = 'auto';
 var DEFAULT_SCALE_DELTA = 1.1;
 var UNKNOWN_SCALE = 0;
