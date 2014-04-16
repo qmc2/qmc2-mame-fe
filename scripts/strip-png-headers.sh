0b68c3cf6b5dbfab91cc22fcb1664a958dd4fda6
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2014-04-16 12:22:07 +0000

    fix: exclude PDF.js's images

--- scripts/strip-png-headers.sh
+++ scripts/strip-png-headers.sh
@@ -2,7 +2,7 @@
 #
 # Please run this AFTER 'make distclean'!
 #
-for i in `find . -type f | egrep ".png$"`; do
+for i in `find . -type f | grep -v "data/js/pdfjs" | egrep ".png$"`; do
 	echo $i
 	convert "$i" -strip "$i"
 done
