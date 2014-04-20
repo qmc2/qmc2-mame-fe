629fb9567f369aa9097f02dd9f76d7cddd92d905
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2014-04-20 11:21:35 +0000

    imp: repeated previous change for QMC2 Arcade's InfoProvider

--- trunk/arcade/infoprovider.h
+++ trunk/arcade/infoprovider.h
@@ -2,6 +2,7 @@
 #define INFOPROVIDER_H
 
 #include <QMap>
+#include <QMultiMap>
 #include <QString>
 
 class InfoProvider
@@ -13,6 +14,11 @@ public:
     enum InfoClass { InfoClassGame, InfoClassEmu };
     QString requestInfo(const QString &, InfoClass);
 
+    bool isMessGameInfo(QString setName) { return qmc2GameInfoSourceMap.values("MESS").contains(setName); }
+    bool isMameGameInfo(QString setName) { return qmc2GameInfoSourceMap.values("MAME").contains(setName); }
+
+    QString &messWikiToHtml(QString &);
+
 private:
     void clearGameInfoDB();
     void clearEmuInfoDB();
@@ -21,6 +27,7 @@ private:
     bool qmc2InfoStopParser;
     QMap<QString, QByteArray *> qmc2EmuInfoDB;
     QMap<QString, QByteArray *> qmc2GameInfoDB;
+    QMultiMap<QString, QString> qmc2GameInfoSourceMap;
     QString urlSectionRegExp;
 };
 
