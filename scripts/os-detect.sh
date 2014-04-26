9b5fb2762a458b87ca45a5b72f5db37546939574
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2014-04-26 16:27:35 +0000

    fix: corrected distro-detection for Fedora (hopefully)

--- scripts/os-detect.sh
+++ scripts/os-detect.sh
@@ -7,15 +7,17 @@ DIST="`uname -r`"
 
 if [ "${OS}" = "Linux" ] ; then
   if [ -f /etc/os-release ] ; then
-    DIST="`cat /etc/os-release | tr '\n' ' ' | sed 's/\ VERSION_ID.*//' | sed 's/\ (.*)//' | sed 's/NAME=//g' | sed 's/VERSION=//g' | sed 's/\"//g'`"
+    VERSION=$(cat /etc/os-release | egrep "\bVERSION_ID\b" | sed 's/VERSION_ID=//g' | sed 's/\"//g')
+    NAME=$(cat /etc/os-release | egrep "\bNAME\b"  | sed 's/NAME=//g' | sed 's/\"//g')
+    DIST="$(echo $NAME)_$(echo $VERSION)"
   elif [ -f /etc/mandriva-release ] ; then
-    DIST="`cat /etc/mandriva-release | sed 's/\ for .*//' | sed 's/\ (.*)//'`"
+    DIST="$(cat /etc/mandriva-release | sed 's/\ for .*//' | sed 's/\ (.*)//')"
   elif [ -f /etc/redhat-release ] ; then
-    DIST="`cat /etc/redhat-release | sed 's/\ (.*)//'`"
+    DIST="$(cat /etc/redhat-release | sed 's/\ (.*)//')"
   elif [ -f /etc/SuSE-release ] ; then
-    DIST="`cat /etc/SuSE-release | tr '\n' ' ' | sed 's/\ VERSION.*//' | sed 's/\ (.*)//'`"
+    DIST="$(cat /etc/SuSE-release | tr '\n' ' ' | sed 's/\ VERSION.*//' | sed 's/\ (.*)//')"
   elif [ -f /etc/debian_version ] ; then
-    DIST="`echo Debian` `cat /etc/debian_version | sed 's/\//-/'`"
+    DIST="$(echo Debian) $(cat /etc/debian_version | sed 's/\//-/')"
   fi
 fi
 
