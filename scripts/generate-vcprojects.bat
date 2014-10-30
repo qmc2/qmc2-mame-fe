af7a0ca5fd0048e40a5db1213802cc2775f8c04e
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2014-10-30 10:52:41 +0000

    imp: version bump in VC++ project generation script

--- scripts/generate-vcprojects.bat
+++ scripts/generate-vcprojects.bat
@@ -29,7 +29,7 @@ REM #################################################
 REM # !!! DON'T CHANGE ANYTHING BELOW THIS LINE !!! #
 REM #################################################
 
-set VERSION=0.45
+set VERSION=0.46
 
 set SVN_REV=0
 set SVN_REV_TEMPLATE=scripts\subwcrev.template
