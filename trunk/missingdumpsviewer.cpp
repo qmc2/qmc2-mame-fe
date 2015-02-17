92f182303a13a156c216664d416292dc7e53a191
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-02-17 15:25:04 +0000

    imp: separated 'missing dumps viewer' settings for system- and software- mode

--- trunk/missingdumpsviewer.cpp
+++ trunk/missingdumpsviewer.cpp
@@ -4,21 +4,28 @@
 
 extern Settings *qmc2Config;
 
-MissingDumpsViewer::MissingDumpsViewer(QWidget *parent)
+MissingDumpsViewer::MissingDumpsViewer(QString settingsKey, QWidget *parent)
 #if defined(QMC2_OS_WIN)
 	: QDialog(parent, Qt::Dialog)
 #else
 	: QDialog(parent, Qt::Dialog | Qt::SubWindow)
 #endif
 {
+	m_settingsKey = settingsKey;
 	setVisible(false);
 	setupUi(this);
 }
 
+void MissingDumpsViewer::on_toolButtonExportToDataFile_clicked()
+{
+	// FIXME
+}
+
 void MissingDumpsViewer::showEvent(QShowEvent *e)
 {
-	restoreGeometry(qmc2Config->value(QMC2_FRONTEND_PREFIX + "Layout/MissingDumpsViewer/Geometry", QByteArray()).toByteArray());
-	treeWidget->header()->restoreState(qmc2Config->value(QMC2_FRONTEND_PREFIX + "Layout/MissingDumpsViewer/HeaderState", QByteArray()).toByteArray());
+	restoreGeometry(qmc2Config->value(QMC2_FRONTEND_PREFIX + "Layout/" + m_settingsKey + "/Geometry", QByteArray()).toByteArray());
+	treeWidget->header()->restoreState(qmc2Config->value(QMC2_FRONTEND_PREFIX + "Layout/" + m_settingsKey + "/HeaderState", QByteArray()).toByteArray());
+	checkBoxSelectedDumpsOnly->setChecked(qmc2Config->value(QMC2_FRONTEND_PREFIX + m_settingsKey + "/SelectedDumpsOnly", false).toBool());
 	if ( e )
 		QDialog::showEvent(e);
 }
@@ -32,8 +39,9 @@ void MissingDumpsViewer::hideEvent(QHideEvent *e)
 
 void MissingDumpsViewer::closeEvent(QCloseEvent *e)
 {
-	qmc2Config->setValue(QMC2_FRONTEND_PREFIX + "Layout/MissingDumpsViewer/Geometry", saveGeometry());
-	qmc2Config->setValue(QMC2_FRONTEND_PREFIX + "Layout/MissingDumpsViewer/HeaderState", treeWidget->header()->saveState());
+	qmc2Config->setValue(QMC2_FRONTEND_PREFIX + "Layout/" + m_settingsKey + "/Geometry", saveGeometry());
+	qmc2Config->setValue(QMC2_FRONTEND_PREFIX + "Layout/" + m_settingsKey + "/HeaderState", treeWidget->header()->saveState());
+	qmc2Config->setValue(QMC2_FRONTEND_PREFIX + m_settingsKey + "/SelectedDumpsOnly", checkBoxSelectedDumpsOnly->isChecked());
 	if ( e )
 		QDialog::closeEvent(e);
 }
