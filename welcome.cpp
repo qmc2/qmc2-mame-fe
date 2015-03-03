5698d7e5d133cf823e05ad0f1f579b589c5d85b3
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-03-03 12:41:06 +0000

    imp: code clean up

--- welcome.cpp
+++ welcome.cpp
@@ -298,62 +298,6 @@ bool Welcome::checkConfig()
 	if ( verList.count() > 1 ) {
 		int omv = verList[1].toInt();
 		int osr = startupConfig->value("SVN_Revision").toInt();
-		if ( QMC2_TEST_VERSION(omv, 46, osr, 6280) ) {
-			// remove the old software-list xml-cache file and the deprecated "FilesAndDirectories/SoftwareListCache" settings keys
-			if ( startupConfig->contains(QMC2_EMULATOR_PREFIX_MAME + "FilesAndDirectories/SoftwareListCache") ) {
-				QFile f(startupConfig->value(QMC2_EMULATOR_PREFIX_MAME + "FilesAndDirectories/SoftwareListCache").toString());
-				if ( f.exists() )
-					f.remove();
-				startupConfig->remove(QMC2_EMULATOR_PREFIX_MAME + "FilesAndDirectories/SoftwareListCache");
-			}
-			if ( startupConfig->contains(QMC2_EMULATOR_PREFIX_MESS + "FilesAndDirectories/SoftwareListCache") ) {
-				QFile f(startupConfig->value(QMC2_EMULATOR_PREFIX_MESS + "FilesAndDirectories/SoftwareListCache").toString());
-				if ( f.exists() )
-					f.remove();
-				startupConfig->remove(QMC2_EMULATOR_PREFIX_MESS + "FilesAndDirectories/SoftwareListCache");
-			}
-			if ( startupConfig->contains(QMC2_EMULATOR_PREFIX_UME + "FilesAndDirectories/SoftwareListCache") ) {
-				QFile f(startupConfig->value(QMC2_EMULATOR_PREFIX_UME + "FilesAndDirectories/SoftwareListCache").toString());
-				if ( f.exists() )
-					f.remove();
-				startupConfig->remove(QMC2_EMULATOR_PREFIX_UME + "FilesAndDirectories/SoftwareListCache");
-			}
-		}
-		if ( QMC2_TEST_VERSION(omv, 46, osr, 6295) ) {
-			// rename old action name 'actionROMAlyzer' to 'actionSystemROMAlyzer'
-			if ( startupConfig->contains(QMC2_FRONTEND_PREFIX_MAME + "Layout/MainWidget/ToolBarActions") ) {
-				QStringList sl = startupConfig->value(QMC2_FRONTEND_PREFIX_MAME + "Layout/MainWidget/ToolBarActions").toStringList();
-				int index = sl.indexOf("actionROMAlyzer");
-				if ( index >= 0 )
-					sl[index] = "actionSystemROMAlyzer";
-				startupConfig->setValue(QMC2_FRONTEND_PREFIX_MAME + "Layout/MainWidget/ToolBarActions", sl);
-			}
-			if ( startupConfig->contains(QMC2_FRONTEND_PREFIX_MESS + "Layout/MainWidget/ToolBarActions") ) {
-				QStringList sl = startupConfig->value(QMC2_FRONTEND_PREFIX_MESS + "Layout/MainWidget/ToolBarActions").toStringList();
-				int index = sl.indexOf("actionROMAlyzer");
-				if ( index >= 0 )
-					sl[index] = "actionSystemROMAlyzer";
-				startupConfig->setValue(QMC2_FRONTEND_PREFIX_MESS + "Layout/MainWidget/ToolBarActions", sl);
-			}
-			if ( startupConfig->contains(QMC2_FRONTEND_PREFIX_UME + "Layout/MainWidget/ToolBarActions") ) {
-				QStringList sl = startupConfig->value(QMC2_FRONTEND_PREFIX_UME + "Layout/MainWidget/ToolBarActions").toStringList();
-				int index = sl.indexOf("actionROMAlyzer");
-				if ( index >= 0 )
-					sl[index] = "actionSystemROMAlyzer";
-				startupConfig->setValue(QMC2_FRONTEND_PREFIX_UME + "Layout/MainWidget/ToolBarActions", sl);
-			}
-		}
-		if ( QMC2_TEST_VERSION(omv, 46, osr, 6310) ) {
-			// remove obsolete "Compress*Info*" keys
-			foreach (QString cKey, QStringList() << "GUI/CompressSoftwareInfoDB" << "FilesAndDirectories/CompressMessInfoDat" << "FilesAndDirectories/CompressMameInfoDat" << "FilesAndDirectories/CompressMessSysinfoDat" << "FilesAndDirectories/CompressMameHistoryDat") {
-				if ( startupConfig->contains(QMC2_FRONTEND_PREFIX_MAME + cKey) )
-					startupConfig->remove(QMC2_FRONTEND_PREFIX_MAME + cKey);
-				if ( startupConfig->contains(QMC2_FRONTEND_PREFIX_MESS + cKey) )
-					startupConfig->remove(QMC2_FRONTEND_PREFIX_MESS + cKey);
-				if ( startupConfig->contains(QMC2_FRONTEND_PREFIX_UME + cKey) )
-					startupConfig->remove(QMC2_FRONTEND_PREFIX_UME + cKey);
-			}
-		}
 #if defined(QMC2_OS_MAC)
 		if ( QMC2_TEST_VERSION(omv, 47, osr, 6350) ) {
 			// changed meaning of Command+Q / Command+X no longer used
