e46fc9ada2ed91f4dd13f9d91217bf5638f43580
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-05-23 10:04:58 +0000

    imp: updated category.ini

--- scripts/cleanup-category-ini.sh
+++ scripts/cleanup-category-ini.sh
@@ -2,8 +2,8 @@
 #
 # Important notes:
 #
-# 1) This script *assumes* that 'qmc2-sdlmess' has been run prior to using it and that
-#    its XML cache database is *up-to-date* and stored as ~/.qmc2/mess-xml-cache.db.
+# 1) This script *assumes* that 'qmc2-sdlmame' has been run prior to using it and that
+#    its XML cache database is *up-to-date* and stored as ~/.qmc2/mame-xml-cache.db.
 #
 # 2) It *requires* the command 'sqlite3', so make sure it's available!
 #
@@ -13,13 +13,13 @@ dos2unix -q /tmp/category.ini
 sed 's/\s*$//g' /tmp/category.ini > /tmp/category.ini.new
 rm /tmp/category.ini
 mv /tmp/category.ini.new /tmp/category.ini
-emuVersion=$(sqlite3 ~/.qmc2/mess-xml-cache.db "select emu_version from mess_xml_cache_metadata")
-dateString="'Updated $(date "+%d-%b-%Y" | tr "[a-z]" "[A-Z]") (MESS $emuVersion)'"
+emuVersion=$(sqlite3 ~/.qmc2/mame-xml-cache.db "select emu_version from mame_xml_cache_metadata")
+dateString="'Updated $(date "+%d-%b-%Y" | tr "[a-z]" "[A-Z]") (MAME $emuVersion)'"
 sed "1s/^.*$/$dateString/g" /tmp/category.ini > /tmp/category.ini.new
 rm /tmp/category.ini
 mv /tmp/category.ini.new /tmp/category.ini
 for i in $(cat /tmp/category.ini | grep -v "^\\[" | grep -v "^tr\\[" | grep "^[0-9a-z]"); do
-	j=$(sqlite3 ~/.qmc2/mess-xml-cache.db "select id from mess_xml_cache where id='$i'")
+	j=$(sqlite3 ~/.qmc2/mame-xml-cache.db "select id from mame_xml_cache where id='$i'")
 	if [ "$j" != "$i" ]; then
 		grep -v "^${i}$" /tmp/category.ini > /tmp/category.ini.new
 		rm /tmp/category.ini
