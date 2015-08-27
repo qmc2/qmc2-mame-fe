62c62c5f881c48bb29d95da9fa756a9c81383b7e
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-08-27 11:54:47 +0000

    imp: nicer syntax

--- trunk/scripts/strip-png-headers.sh
+++ trunk/scripts/strip-png-headers.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#
-# Please run this AFTER 'make distclean'!
-#
-for i in `find . -type f | grep -v "data/js/pdfjs" | egrep ".png$"`; do
+for i in $(find . -type f | grep -v "data/js/pdfjs" | egrep ".png$"); do
 	echo $i
 	convert "$i" -strip "$i"
 done
