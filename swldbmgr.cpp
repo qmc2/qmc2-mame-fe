e405e34aea192f8a09766dbf68cfe54c14153e3f
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-09-21 09:57:18 +0000

    imp: slightly optimized exists() look-ups

--- swldbmgr.cpp
+++ swldbmgr.cpp
@@ -422,15 +422,12 @@ QString SoftwareListXmlDatabaseManager::idAtIndex(int index)
 bool SoftwareListXmlDatabaseManager::exists(QString list, QString id)
 {
 	QSqlQuery query(m_db);
-	query.prepare(QString("SELECT list, id FROM %1 WHERE list=:list AND id=:id").arg(m_tableBasename));
+	query.prepare(QString("SELECT list, id FROM %1 WHERE list=:list AND id=:id LIMIT 1").arg(m_tableBasename));
 	query.bindValue(":list", list);
 	query.bindValue(":id", id);
-	if ( query.exec() ) {
-		if ( query.first() )
-			return (query.value(0).toString() == list) && (query.value(1).toString() == id);
-		else
-			return false;
-	} else {
+	if ( query.exec() )
+		return query.first();
+	else {
 		qmc2MainWindow->log(QMC2_LOG_FRONTEND, tr("WARNING: failed to fetch '%1' from software-list XML cache database: query = '%2', error = '%3'").arg("list, id").arg(query.lastQuery()).arg(m_db.lastError().text()));
 		return false;
 	}
