78ce996b1ac0ca19f848716ba1f144774b310cc1
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-10-10 08:54:13 +0000

    imp: added "System / BIOS" as a constant category to the arcade mode setup dialog in order to allow for filtering of BIOS sets even if neither catver.ini nor category.ini contain this category

--- arcademodesetup.cpp
+++ arcademodesetup.cpp
@@ -499,6 +499,9 @@ void ArcadeModeSetup::updateCategoryFilter()
 	QListWidgetItem *item = new QListWidgetItem(tr("?"), listWidgetCategoryFilter);
 	item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
 	item->setCheckState(excludedCategories.contains(tr("?")) ? Qt::Unchecked : Qt::Checked);
+	item = new QListWidgetItem(tr("System / BIOS"), listWidgetCategoryFilter);
+	item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsUserCheckable);
+	item->setCheckState(excludedCategories.contains(tr("System / BIOS")) ? Qt::Unchecked : Qt::Checked);
 	foreach (QString category, categoryNames) {
 		if ( !category.isEmpty() ) {
 			item = new QListWidgetItem(category, listWidgetCategoryFilter);
