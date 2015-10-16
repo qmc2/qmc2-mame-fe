c78a24663379883ec2d71d0b647ac9d51a5605e9
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-10-16 13:25:49 +0000

    impo: code clean up (indentation corrected, method renamed)

--- arcade/datinfodbmgr.h
+++ arcade/datinfodbmgr.h
@@ -11,25 +11,25 @@ class DatInfoDatabaseManager : public QObject
 	Q_OBJECT
 
 	public:
-		explicit DatInfoDatabaseManager(QObject *parent);
-		~DatInfoDatabaseManager();
+        explicit DatInfoDatabaseManager(QObject *parent);
+        ~DatInfoDatabaseManager();
 
-		QString qmc2Version();
-		void setQmc2Version(QString qmc2_version);
-		int datInfoVersion();
-		void setDatInfoVersion(int datinfo_version);
+        QString qmc2Version();
+        void setQmc2Version(QString qmc2_version);
+        int datInfoVersion();
+        void setDatInfoVersion(int datinfo_version);
 
-		QString softwareInfo(QString list, QString id);
-		void setSoftwareInfo(QString list, QString id, QString infotext);
-		bool existsSoftwareInfo(QString list, QString id);
-		qint64 softwareInfoRowCount();
-		bool softwareInfoImportRequired(QStringList pathList);
+        QString softwareInfo(QString list, QString id);
+        void setSoftwareInfo(QString list, QString id, QString infotext);
+        bool existsSoftwareInfo(QString list, QString id);
+        qint64 softwareInfoRowCount();
+        bool softwareInfoImportRequired(QStringList pathList);
 
-		QString emuInfo(QString id);
-		void setEmuInfo(QString id, QString infotext);
-		bool existsEmuInfo(QString id);
-		qint64 emuInfoRowCount();
-		bool emuInfoImportRequired(QStringList pathList);
+        QString emuInfo(QString id);
+        void setEmuInfo(QString id, QString infotext);
+        bool existsEmuInfo(QString id);
+        qint64 emuInfoRowCount();
+        bool emuInfoImportRequired(QStringList pathList);
 
         QString machineInfo(QString id);
         QString machineInfoEmulator(QString id);
@@ -38,34 +38,34 @@ class DatInfoDatabaseManager : public QObject
         qint64 machineInfoRowCount();
         bool machineInfoImportRequired(QStringList pathList);
 
-		QString connectionName() { return m_connectionName; }
-		QString databasePath() { return m_db.databaseName(); }
-		quint64 databaseSize();
-		void setCacheSize(quint64 kiloBytes);
-		void setSyncMode(uint syncMode);
-		void setJournalMode(uint journalMode);
+        QString connectionName() { return m_connectionName; }
+        QString databasePath() { return m_db.databaseName(); }
+        quint64 databaseSize();
+        void setCacheSize(quint64 kiloBytes);
+        void setSyncMode(uint syncMode);
+        void setJournalMode(uint journalMode);
 
-        void updateDatabaseFormat(int from, int to);
+        void upgradeDatabaseFormat(int from, int to);
 
-	public slots:
-		void recreateSoftwareInfoTable();
-		void recreateEmuInfoTable();
+    public slots:
+        void recreateSoftwareInfoTable();
+        void recreateEmuInfoTable();
         void recreateMachineInfoTable();
-		void recreateMetaDataTable();
-		void recreateDatabase();
-		void importSoftwareInfo(QStringList pathList, bool fromScratch = true);
-		void importEmuInfo(QStringList pathList, bool fromScratch = true);
+        void recreateMetaDataTable();
+        void recreateDatabase();
+        void importSoftwareInfo(QStringList pathList, bool fromScratch = true);
+        void importEmuInfo(QStringList pathList, bool fromScratch = true);
         void importMachineInfo(QStringList pathList, QStringList emulatorList, bool fromScratch = true);
-		void beginTransaction() { m_db.driver()->beginTransaction(); }
-		void commitTransaction() { m_db.driver()->commitTransaction(); }
+        void beginTransaction() { m_db.driver()->beginTransaction(); }
+        void commitTransaction() { m_db.driver()->commitTransaction(); }
 
-	private:
-		mutable QSqlDatabase m_db;
-		QString m_softwareInfoTableName;
-		QString m_emuInfoTableName;
+    private:
+        mutable QSqlDatabase m_db;
+        QString m_softwareInfoTableName;
+        QString m_emuInfoTableName;
         QString m_machineInfoTableName;
-		QString m_metaDataTableName;
-		QString m_connectionName;
+        QString m_metaDataTableName;
+        QString m_connectionName;
 };
 
 #endif
