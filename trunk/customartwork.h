7667433fcb15b345e9c93f820f0430135207f6e1
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-11-11 09:43:53 +0000

    wip: added framework for upcoming CustomArtwork class

---
+++ trunk/customartwork.h
@@ -0,0 +1,30 @@
+#ifndef _CUSTOMARTWORK_H_
+#define _CUSTOMARTWORK_H_
+
+#include <QString>
+
+#include "imagewidget.h"
+
+class CustomArtwork : public ImageWidget
+{
+	Q_OBJECT 
+
+	public:
+		CustomArtwork(QWidget *parent);
+
+		virtual QString cachePrefix() { return m_cachePrefix; }
+		virtual QString imageZip();
+		virtual QString imageDir();
+		virtual QString imageType() { return m_imageType; }
+		virtual int imageTypeNumeric() { return m_imageTypeNumeric; }
+		virtual bool useZip();
+		virtual bool useSevenZip();
+		virtual bool scaledImage();
+
+	private:
+		QString m_cachePrefix;
+		QString m_imageType;
+		int m_imageTypeNumeric;
+};
+
+#endif
