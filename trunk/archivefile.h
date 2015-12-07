8c226fac9f567f0a6d4402aa13190969f63bbb83
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2015-12-07 20:04:49 +0000

    fix: supplemental to previous change

--- trunk/archivefile.h
+++ trunk/archivefile.h
@@ -15,57 +15,57 @@
 
 class ArchiveEntryMetaData
 {
-public:
-	explicit ArchiveEntryMetaData(QString name = QString(), quint64 size = 0, QDateTime date = QDateTime())
-	{
-		setName(name);
-		setSize(size);
-		setDate(date);
-	}
+	public:
+		explicit ArchiveEntryMetaData(QString name = QString(), quint64 size = 0, QDateTime date = QDateTime())
+		{
+			setName(name);
+			setSize(size);
+			setDate(date);
+		}
 
-	void setName(QString name) { m_name = name; }
-	QString name() { return m_name; }
-	void setSize(quint64 size) { m_size = size; }
-	quint64 size() { return m_size; }
-	void setDate(QDateTime date) { m_date = date; }
-	QDateTime date() { return m_date; }
+		void setName(QString name) { m_name = name; }
+		QString name() { return m_name; }
+		void setSize(quint64 size) { m_size = size; }
+		quint64 size() { return m_size; }
+		void setDate(QDateTime date) { m_date = date; }
+		QDateTime date() { return m_date; }
 
-private:
-	QString m_name;
-	quint64 m_size;
-	QDateTime m_date;
+	private:
+		QString m_name;
+		quint64 m_size;
+		QDateTime m_date;
 };
 
 class ArchiveFile : public QObject
 {
 	Q_OBJECT
 
-public:
-	explicit ArchiveFile(QString fileName = QString(), QObject *parent = 0);
-	~ArchiveFile();
+	public:
+		explicit ArchiveFile(QString fileName = QString(), QObject *parent = 0);
+		~ArchiveFile();
 
-	QString fileName() { return m_fileName; }
-	QList<ArchiveEntryMetaData> &entryList() { return m_entryList; }
-	bool isOpen() { return m_fd >= 0; }
-	bool open(QString fileName = QString());
-	void close();
-	bool seekNextEntry(ArchiveEntryMetaData *metaData, bool *reset);
-	bool seekEntry(QString name);
-	bool seekEntry(uint index);
-	qint64 readBlock(QByteArray *buffer);
-	QString errorString();
-	int errorCode();
+		QString fileName() { return m_fileName; }
+		QList<ArchiveEntryMetaData> &entryList() { return m_entryList; }
+		bool isOpen() { return m_fd >= 0; }
+		bool open(QString fileName = QString());
+		void close();
+		bool seekNextEntry(ArchiveEntryMetaData *metaData, bool *reset);
+		bool seekEntry(QString name);
+		bool seekEntry(uint index);
+		qint64 readBlock(QByteArray *buffer);
+		QString errorString();
+		int errorCode();
 
-private:
-	void createItemList();
-	int indexOfName(QString name);
+	private:
+		void createItemList();
+		int indexOfName(QString name);
 
-	struct archive *m_archive;
-	int m_fd;
-	const void *m_buffer;
-	QFile m_file;
-	QList<ArchiveEntryMetaData> m_entryList;
-	QString m_fileName;
+		struct archive *m_archive;
+		int m_fd;
+		const void *m_buffer;
+		QFile m_file;
+		QList<ArchiveEntryMetaData> m_entryList;
+		QString m_fileName;
 };
 
 #endif
