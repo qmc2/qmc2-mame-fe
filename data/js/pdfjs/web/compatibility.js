fd74c4aadc98867f016062ce81d7995ed9b47bb9
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-01-01 09:31:47 +0000

    imp: updated PDF.js to latest git revision

--- data/js/pdfjs/web/compatibility.js
+++ data/js/pdfjs/web/compatibility.js
@@ -1,5 +1,3 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
-/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 /* Copyright 2012 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -576,4 +574,20 @@ if (typeof PDFJS === 'undefined') {
   if (isEmbeddedIE) {
     PDFJS.disableFullscreen = true;
   }
+})();
+
+// Provides document.currentScript support
+// Support: IE, Chrome<29.
+(function checkCurrentScript() {
+  if ('currentScript' in document) {
+    return;
+  }
+  Object.defineProperty(document, 'currentScript', {
+    get: function () {
+      var scripts = document.getElementsByTagName('script');
+      return scripts[scripts.length - 1];
+    },
+    enumerable: true,
+    configurable: true
+  });
 })();
