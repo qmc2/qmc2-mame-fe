f01efd5a082fc6045a1d0f7b682baf37bdac0486
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-02-05 14:35:44 +0000

    imp: update emulator configuration templates - part III (all new options added)
    new: added 'color' type / editor for new UI options to the emulator
    configuration

--- colorwidget.h
+++ colorwidget.h
@@ -4,6 +4,7 @@
 #include <QPalette>
 #include <QSize>
 #include <QString>
+#include <QChar>
 #include <QFontMetrics>
 #include "ui_colorwidget.h"
 
@@ -13,7 +14,6 @@ class ColorWidget : public QWidget, public Ui::ColorWidget
 
        	public:
 		ColorWidget(QString, QString, QPalette::ColorGroup, QPalette::ColorRole, QColor, QBrush, QWidget *parent = 0, bool showBrushButton = true, bool simpleTxt = false);
-		~ColorWidget();
 
 		QPalette::ColorGroup colorGroup;
 		QPalette::ColorRole colorRole;
@@ -29,8 +29,25 @@ class ColorWidget : public QWidget, public Ui::ColorWidget
 			return QSize(50, fm.height() + 6);
 		}
 
+		QString argbValue()
+		{
+			int r, g, b, a;
+			activeColor.getRgb(&r, &g, &b, &a);
+			return QString("%1%2%3%4").arg(a, 2, 16, QChar('0')).arg(r, 2, 16, QChar('0')).arg(g, 2, 16, QChar('0')).arg(b, 2, 16, QChar('0'));
+		}
+
+		void setArgb(QString argb)
+		{
+			int a = argb.mid(0, 2).toInt(0, 16);
+			int r = argb.mid(2, 2).toInt(0, 16);
+			int g = argb.mid(4, 2).toInt(0, 16);
+			int b = argb.mid(6, 2).toInt(0, 16);
+			activeColor.setRgb(r, g, b, a);
+		}
+
 	public slots:
 		void adjustIconSizes();
+		void updateColor();
 		void on_toolButtonColor_clicked();
 		void on_toolButtonBrush_clicked();
 
@@ -40,6 +57,7 @@ class ColorWidget : public QWidget, public Ui::ColorWidget
 	signals:
 		void colorChanged(QPalette::ColorGroup, QPalette::ColorRole, QColor);
 		void brushChanged(QPalette::ColorGroup, QPalette::ColorRole, QBrush);
+		void dataChanged();
 };
 
 #endif
