3d8c3d816743ed4c68ebd96d9ed31ac153af9b2c
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-02-28 14:22:46 +0000

    fix: repeated previous fixes for QMC2 Arcade / corrected a new bug

--- arcade/processmanager.h
+++ arcade/processmanager.h
@@ -9,36 +9,36 @@
 
 class ProcessManager : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 public:
-    explicit ProcessManager(QObject *parent = 0);
-    virtual ~ProcessManager();
+	explicit ProcessManager(QObject *parent = 0);
+	virtual ~ProcessManager();
 
-    int startEmulator(QString);
-    void createTemplateList();
+	int startEmulator(QString);
+	void createTemplateList();
 
-    QString fileErrorToString(QFile::FileError);
-    QString processErrorToString(QProcess::ProcessError);
-    QString processStateToString(QProcess::ProcessState);
-    int highestProcessID() { return mCurrentProcessId; }
-    int runningProcesses() { return mProcessMap.count(); }
+	QString fileErrorToString(QFile::FileError);
+	QString processErrorToString(QProcess::ProcessError);
+	QString processStateToString(QProcess::ProcessState);
+	int highestProcessID() { return mCurrentProcessId; }
+	int runningProcesses() { return mProcessMap.count(); }
 
 signals:
-    void emulatorStarted(int);
-    void emulatorFinished(int);
+	void emulatorStarted(int);
+	void emulatorFinished(int);
 
 public slots:
-    void error(QProcess::ProcessError);
-    void finished(int, QProcess::ExitStatus);
-    void readyReadStandardOutput();
-    void readyReadStandardError();
-    void started();
-    void stateChanged(QProcess::ProcessState);
+	void error(QProcess::ProcessError);
+	void finished(int, QProcess::ExitStatus);
+	void readyReadStandardOutput();
+	void readyReadStandardError();
+	void started();
+	void stateChanged(QProcess::ProcessState);
 
 private:
-    int mCurrentProcessId;
-    QMap<int, QProcess *> mProcessMap;
-    QList<EmulatorOption> mTemplateList;
+	int mCurrentProcessId;
+	QMap<int, QProcess *> mProcessMap;
+	QList<EmulatorOption> mTemplateList;
 };
 
 #endif // PROCESSMANAGER_H
