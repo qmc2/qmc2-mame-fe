c6bf8ad3a6a0dadf452be5eb330335adf528e775
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-03-09 11:50:48 +0000

    imp: removed unused / unnecessary parameters from MachineList::loadIcon()

--- machinelist.h
+++ machinelist.h
@@ -131,7 +131,7 @@ class MachineList : public QObject
 		void insertAttributeItems(QList<QTreeWidgetItem *> *itemList, QString element, QStringList attributes, QStringList descriptions, bool translate = false);
 		void enableWidgets(bool enable = true);
 		void filter(bool initial = false);
-		bool loadIcon(QString, QTreeWidgetItem *, bool checkOnly = false, QString *fileName = 0);
+		bool loadIcon(QString, QTreeWidgetItem *item);
 
 	private:
 		XmlDatabaseManager *m_xmlDb;
