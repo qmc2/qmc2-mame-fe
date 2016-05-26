2930384b2f812477388306f864ea0cbbe1854283
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-05-26 09:01:59 +0000

    imp: bumped version

--- trunk/scripts/generate-vcprojects.bat
+++ trunk/scripts/generate-vcprojects.bat
@@ -32,7 +32,7 @@ REM #################################################
 REM # !!! DON'T CHANGE ANYTHING BELOW THIS LINE !!! #
 REM #################################################
 
-set VERSION=0.63
+set VERSION=0.64
 
 set SVN_REV=0
 set SVN_REV_TEMPLATE=scripts\subwcrev.template
