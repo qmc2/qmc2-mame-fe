d22f49068dd932c3c247f2de355e344807276dbe
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-05-27 14:56:14 +0000

    fix: correct Windows build fix :)

--- trunk/collectionrebuilder.cpp
+++ trunk/collectionrebuilder.cpp
@@ -1669,7 +1669,7 @@ bool CollectionRebuilderThread::hardlinkChds(QString baseDir, QString id, QStrin
 				if ( f.exists() )
 					f.remove();
 #if defined(QMC2_OS_WIN)
-				success = CreateHardLink(fileName.toUtf8().data(), path.toUtf8().data(), NULL);
+				success = CreateHardLink((LPCTSTR)fileName.utf16(), (LPCTSTR)path.utf16(), NULL);
 #else
 				success = ::link(path.toUtf8().constData(), fileName.toUtf8().constData()) == 0;
 #endif
