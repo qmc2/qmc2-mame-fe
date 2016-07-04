d70bd141b392f4085b3cf47c03dac19a328c1a5d
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-07-04 10:09:29 +0000

    wip: split .h and .cpp

--- machinelistmodel.h
+++ machinelistmodel.h
@@ -8,7 +8,7 @@
 #include <QVariant>
 #include <QIcon>
 
-//#include "macros.h"
+#include "macros.h"
 
 #define QMC2_MLM_ROM_TYPE_NONE			0
 #define QMC2_MLM_ROM_TYPE_ROM			1
@@ -23,44 +23,9 @@
 class MachineListModelItem
 {
 	public:
-		MachineListModelItem(const QString &name, const QIcon &icon, const QString &parent, const QString &description, const QString &manufacturer, const QString &year, const QString &source_file, const QString &players, const QString &category, const QString &version, int rank, char rom_status, char rom_types, char driver_status, bool is_device, bool is_bios, bool tagged, MachineListModelItem *parentItem = 0) :
-			m_parentItem(parentItem)
-		{
-			setName(name);
-			setParent(parent);
-			setDescription(description);
-			setManufacturer(manufacturer);
-			setYear(year);
-			setSourceFile(source_file);
-			setPlayers(players);
-			setCategory(category);
-			setVersion(version);
-			setIcon(icon);
-			setRank(rank);
-			setRomStatus(rom_status);
-			setRomTypes(rom_types);
-			setDriverStatus(driver_status);
-			setIsDevice(is_device);
-			setIsBios(is_bios);
-			setTag(tagged);
-		}
-
-		MachineListModelItem(MachineListModelItem *parentItem = 0) :
-			m_parentItem(parentItem)
-		{
-			setRank(0);
-			setRomStatus('U');
-			setRomTypes(QMC2_MLM_ROM_TYPE_NONE);
-			setDriverStatus(QMC2_MLM_DRIVER_STATUS_NONE);
-			setIsDevice(false);
-			setIsBios(false);
-			setTag(false);
-		}
-
-		~MachineListModelItem()
-		{
-			qDeleteAll(childItems());
-		}
+		MachineListModelItem(const QString &name, const QIcon &icon, const QString &parent, const QString &description, const QString &manufacturer, const QString &year, const QString &source_file, const QString &players, const QString &category, const QString &version, int rank, char rom_status, char rom_types, char driver_status, bool is_device, bool is_bios, bool tagged, MachineListModelItem *parentItem = 0);
+		MachineListModelItem(MachineListModelItem *parentItem = 0);
+		~MachineListModelItem();
 
 		void setName(const QString &name) { m_name = name; }
 		QString &name() { return m_name; }
@@ -128,35 +93,11 @@ class MachineListProxyModel : public QSortFilterProxyModel
 	Q_OBJECT
 
 	public:
-		MachineListProxyModel(QObject *parent = 0) :
-			QSortFilterProxyModel(parent)
-		{
-			// NOP
-		}
+		MachineListProxyModel(QObject *parent = 0);
 
 	protected:
-		bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
-		{
-			// FIXME
-			return true;
-		}
-		bool lessThan(const QModelIndex &left, const QModelIndex &right) const
-		{
-			QVariant leftData(sourceModel()->data(left));
-			QVariant rightData(sourceModel()->data(right));
-			switch ( leftData.type() ) {
-				case QVariant::String:
-					return QString::localeAwareCompare(leftData.toString(), rightData.toString()) < 0;
-				case QVariant::Int:
-					return leftData.toInt() < rightData.toInt();
-				case QVariant::Char:
-					return leftData.toChar() < rightData.toChar();
-				case QVariant::Bool:
-					return leftData.toBool() < rightData.toBool();
-				default:
-					return false;
-			}
-		}
+		bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const;
+		bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
 
 	private:
 };
@@ -168,141 +109,26 @@ class MachineListModel : public QAbstractItemModel
 	public:
 		enum Column {TAG, ICON, NAME, PARENT, DESCRIPTION, MANUFACTURER, YEAR, ROM_STATUS, ROM_TYPES, DRIVER_STATUS, SOURCE_FILE, PLAYERS, RANK, IS_BIOS, IS_DEVICE, CATEGORY, VERSION, LAST_COLUMN};
 
-		MachineListModel(QObject *parent) :
-			QAbstractItemModel(parent),
-			m_rootItem(0)
-		{
-			m_headers << tr("Tag") << tr("Icon") << tr("Name") << tr("Parent") << tr("Description") << tr("Manufacturer") << tr("Year") << tr("ROM Status") << tr("ROM Types") << tr("Driver Status") << tr("Source File") << tr("Players") << tr("Rank") << tr("Is BIOS?") << tr("Is Device?") << tr("Category") << tr("Version");
-		}
+		MachineListModel(QObject *parent = 0);
+		~MachineListModel();
 
-		~MachineListModel()
-		{
-			delete m_rootItem;
-		}
-
-		void setRootItem(MachineListModelItem *item)
-		{
-			delete m_rootItem;
-			m_rootItem = item;
-#if QT_VERSION < 0x050000
-			reset();
-#endif
-		}
+		void setRootItem(MachineListModelItem *item);
  
-		virtual QModelIndex index(int row, int column, const QModelIndex &parent) const
-		{
-			if ( !m_rootItem )
-				return QModelIndex();
-			MachineListModelItem *parentItem = itemFromIndex(parent);
-			return createIndex(row, column, parentItem->childItems().at(row));
-		}
-
-		virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const
-		{
-			if ( orientation == Qt::Horizontal ) {
-				switch ( role ) {
-					case Qt::DisplayRole:
-						return m_headers.at(section);
-					case Qt::TextAlignmentRole:
-						return Qt::AlignLeft;
-				}
-			}
-			return QVariant();
-		}
-
-		/*
-		virtual bool canFetchMore(const QModelIndex &parent) const
-		{
-			// FIXME
-			return false;
-		}
-
-		virtual void fetchMore(const QModelIndex &parent)
-		{
-			// FIXME
-		}
-		*/
-
-		virtual Qt::ItemFlags flags(const QModelIndex &index) const
-		{
-			return Qt::ItemIsSelectable | Qt::ItemIsEnabled | Qt::ItemIsDragEnabled;
-		}
-
-		virtual int columnCount(const QModelIndex &) const
-		{
-			return int(LAST_COLUMN);
-		}
-
-		virtual int rowCount(const QModelIndex &parent = QModelIndex()) const
-		{
-			MachineListModelItem *parentItem = itemFromIndex(parent);
-			if ( !parentItem )
-				return 0;
-			return parentItem->childItems().count();
-		}
-
-		virtual QVariant data(const QModelIndex &index, int role) const
-		{
-			if ( !index.isValid() )
-				return QVariant();
-			MachineListModelItem *item = itemFromIndex(index);
-			if ( !item )
-				return QVariant();
-			QVariant data;
-			switch ( role ) {
-				case Qt::TextAlignmentRole:
-					return Qt::AlignLeft;
-				case Qt::DisplayRole:
-					switch ( Column(index.column()) ) {
-						case TAG:
-							return item->tagged();
-						case ICON:
-							return item->icon();
-						// FIXME
-						case NAME:
-						default:
-							return item->name();
-					}
-					break;
-				case Qt::DecorationRole:
-					switch ( Column(index.column()) ) {
-						// FIXME
-						default:
-							break;
-					}
-					break;
-				default:
-					return QVariant();
-			}
-			return data;
-		}
-
-		virtual QModelIndex parent(const QModelIndex &child) const
-		{
-			MachineListModelItem *item = itemFromIndex(child);
-			if ( !item )
-				return QModelIndex();
-			MachineListModelItem *parentItem = item->parentItem();
-			if ( !parentItem )
-				return QModelIndex();
-			MachineListModelItem *grandParentItem = parentItem->parentItem();
-			if ( !grandParentItem )
-				return QModelIndex();
-			int row = grandParentItem->childItems().indexOf(parentItem);
-			return createIndex(row, child.column(), parentItem);
-		}
+		virtual QModelIndex index(int row, int column, const QModelIndex &parent) const;
+		virtual QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+		//virtual bool canFetchMore(const QModelIndex &parent) const;
+		//virtual void fetchMore(const QModelIndex &parent) const;
+		virtual Qt::ItemFlags flags(const QModelIndex &index) const;
+		virtual int columnCount(const QModelIndex &) const;
+		virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+		virtual QVariant data(const QModelIndex &index, int role) const;
+		virtual QModelIndex parent(const QModelIndex &child) const;
 
 	private:
 		QStringList m_headers;
 		MachineListModelItem *m_rootItem;
 
-		MachineListModelItem *itemFromIndex(const QModelIndex &index) const
-		{
-			if ( index.isValid() )
-				return static_cast<MachineListModelItem *>(index.internalPointer());
-		        else
-				return m_rootItem;
-		}
+		MachineListModelItem *itemFromIndex(const QModelIndex &index) const;
 };
 
 #endif
