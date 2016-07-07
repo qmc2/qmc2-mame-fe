f01f72c48868feb637a91eb9eced5e81c639ef63
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-07-07 14:39:47 +0000

    wip: adjusted view's icon size / added framework for a machine list view specific item delegate

--- trunk/machinelistviewer.h
+++ trunk/machinelistviewer.h
@@ -1,8 +1,24 @@
 #ifndef _MACHINELISTVIEWER_H_
 #define _MACHINELISTVIEWER_H_
 
+#include <QStyledItemDelegate>
+
 #include "machinelistmodel.h"
 #include "ui_machinelistviewer.h"
+
+class MlvDelegate : public QStyledItemDelegate
+{
+	Q_OBJECT 
+
+	public:
+		MlvDelegate(QWidget *parent = 0) : QStyledItemDelegate(parent) {}
+
+		void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+		QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
+		QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
+		void setEditorData(QWidget *editor, const QModelIndex &index) const;
+		void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
+};
 
 class MachineListViewer : public QWidget, public Ui::MachineListViewer
 {
