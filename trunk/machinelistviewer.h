0a3d769fae6fa4c2ff3ba963f9d1838fcb4511a4
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-07-07 16:25:33 +0000

    wip: reverted previous change partially because it can be done with index-widgets instead of a delegate

--- trunk/machinelistviewer.h
+++ trunk/machinelistviewer.h
@@ -6,20 +6,6 @@
 #include "machinelistmodel.h"
 #include "ui_machinelistviewer.h"
 
-class MlvDelegate : public QStyledItemDelegate
-{
-	Q_OBJECT 
-
-	public:
-		MlvDelegate(QWidget *parent = 0) : QStyledItemDelegate(parent) {}
-
-		void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
-		QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const;
-		QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
-		void setEditorData(QWidget *editor, const QModelIndex &index) const;
-		void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const;
-};
-
 class MachineListViewer : public QWidget, public Ui::MachineListViewer
 {
 	Q_OBJECT
