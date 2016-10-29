c9d421f18438596f6b3714f5187280a2d29df8ea
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2016-10-29 11:28:51 +0000

    imp: updated the os-detection script to also support Arch Linux (also added a distribution specfic configuration file for it)

--- trunk/scripts/os-detect.sh
+++ trunk/scripts/os-detect.sh
@@ -5,47 +5,52 @@ OS="$(uname -s)"
 UNAME="$(uname -a)"
 DIST="$(uname -r)"
 
-if [ "${OS}" = "Linux" ] ; then
-  if [ -f /etc/os-release ] ; then
-    VERSION_ID=$(cat /etc/os-release | egrep "\bVERSION_ID\b" | sed 's/VERSION_ID=//g' | sed 's/\"//g')
-    VERSION=$(cat /etc/os-release | egrep "\bVERSION\b" | sed 's/VERSION=//g' | sed 's/\"//g')
-    NAME=$(cat /etc/os-release | egrep "\bNAME\b" | sed 's/NAME=//g' | sed 's/\"//g' | tr ' ' '_' | tr '/' '_')
-    if [ "${NAME}" = "Fedora" ] ; then
-    	NAME="Fedora_release"
-    	DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
-    elif [ "${NAME}" = "Ubuntu" ] ; then
-    	DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
-    elif [ "${NAME}" = "openSUSE" ] ; then
-        if [ "${VERSION}" != "" ] ; then
-            DIST="$(echo ${NAME})_$(echo ${VERSION})"
-        else
-            DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
-        fi
-    else
-    	DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
-    fi
-  elif [ -f /etc/mandriva-release ] ; then
-    DIST="$(cat /etc/mandriva-release | sed 's/\ for .*//' | sed 's/\ (.*)//')"
-  elif [ -f /etc/redhat-release ] ; then
-    DIST="$(cat /etc/redhat-release | sed 's/\ (.*)//')"
-  elif [ -f /etc/SuSE-release ] ; then
-    DIST="$(cat /etc/SuSE-release | tr '\n' ' ' | sed 's/\ VERSION.*//' | sed 's/\ (.*)//')"
-  elif [ -f /etc/debian_version ] ; then
-    DIST="$(echo Debian) $(cat /etc/debian_version | sed 's/\//-/')"
-  fi
+if [ "${OS}" = "Linux" ]; then
+	if [ -f /etc/os-release ]; then
+		VERSION_ID=$(cat /etc/os-release | egrep "\bVERSION_ID\b" | sed 's/VERSION_ID=//g' | sed 's/\"//g')
+		VERSION=$(cat /etc/os-release | egrep "\bVERSION\b" | sed 's/VERSION=//g' | sed 's/\"//g')
+		NAME=$(cat /etc/os-release | egrep "\bNAME\b" | sed 's/NAME=//g' | sed 's/\"//g' | tr ' ' '_' | tr '/' '_')
+		case ${NAME} in
+			Fedora)
+				NAME="Fedora_release"
+				DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
+				;;
+			Ubuntu)
+				DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
+				;;
+			openSUSE)
+				if [ "${VERSION}" != "" ] ; then
+					DIST="$(echo ${NAME})_$(echo ${VERSION})"
+				else
+					DIST="$(echo ${NAME})_$(echo ${VERSION_ID})"
+				fi
+				;;
+			*)
+				DIST="$(echo ${NAME})"
+				;;
+		esac
+	elif [ -f /etc/mandriva-release ]; then
+		DIST="$(cat /etc/mandriva-release | sed 's/\ for .*//' | sed 's/\ (.*)//')"
+	elif [ -f /etc/redhat-release ]; then
+		DIST="$(cat /etc/redhat-release | sed 's/\ (.*)//')"
+	elif [ -f /etc/SuSE-release ]; then
+		DIST="$(cat /etc/SuSE-release | tr '\n' ' ' | sed 's/\ VERSION.*//' | sed 's/\ (.*)//')"
+	elif [ -f /etc/debian_version ]; then
+		DIST="$(echo Debian) $(cat /etc/debian_version | sed 's/\//-/')"
+	fi
 fi
 
 OSCFG="arch/$(echo ${OS} | sed -e 's/[\ \\\/]/_/g').cfg"
-if [ -f ${OSCFG} ] ; then
-  OSCFG="System cfg-file (ok) ........ `echo ${OSCFG}`"
+if [ -f ${OSCFG} ]; then
+	OSCFG="System cfg-file (ok) ........ `echo ${OSCFG}`"
 else
-  OSCFG="System cfg-file (?) ......... `echo ${OSCFG}`"
+	OSCFG="System cfg-file (?) ......... `echo ${OSCFG}`"
 fi
 DISTCFG="arch/$(echo ${OS} | sed -e 's/[\ \\\/]/_/g')/$(echo ${DIST} | sed -e 's/[\ \\\/]/_/g').cfg"
-if [ -f ${DISTCFG} ] ; then
-  DISTCFG="Distribution cfg-file (ok) .. `echo ${DISTCFG}`"
+if [ -f ${DISTCFG} ]; then
+	DISTCFG="Distribution cfg-file (ok) .. `echo ${DISTCFG}`"
 else
-  DISTCFG="Distribution cfg-file (?) ... `echo ${DISTCFG}`"
+	DISTCFG="Distribution cfg-file (?) ... `echo ${DISTCFG}`"
 fi
 
 echo "Operating System ............ ${OS}"
