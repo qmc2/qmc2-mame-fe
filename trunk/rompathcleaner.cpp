cbf09bd5d6614c6db48ca270e0dae863a25498d2
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2017-05-06 11:00:26 +0000

    wip: added framework for an upcoming "ROM path cleaner" sub-feature of the ROMAlyzer

---
+++ trunk/rompathcleaner.cpp
@@ -0,0 +1,7 @@
+#include "rompathcleaner.h"
+
+RomPathCleaner::RomPathCleaner(QWidget *parent) :
+	QWidget(parent)
+{
+	setupUi(this);
+}
---
+++ trunk/rompathcleaner.h
@@ -0,0 +1,20 @@
+#ifndef _ROMPATHCLEANER_H_
+#define _ROMPATHCLEANER_H_
+
+#include "ui_rompathcleaner.h"
+
+class RomPathCleaner : public QWidget, public Ui::RomPathCleaner
+{
+	Q_OBJECT
+
+       	public:
+		RomPathCleaner(QWidget *parent = 0);
+
+	public slots:
+
+	signals:
+
+	private:
+};
+
+#endif
