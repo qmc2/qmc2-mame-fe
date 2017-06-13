84dfffe8caddf7b052c618146aa674aa4e00c6ed
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2017-06-13 12:45:12 +0000

    wip: SetupWizard updates

--- trunk/setupwizard.h
+++ trunk/setupwizard.h
@@ -2,8 +2,15 @@
 #define _SETUPWIZARD_H
 
 #include <QSettings>
+#include <QString>
 
 #include "ui_setupwizard.h"
+
+#define QMC2_SETUPWIZARD_PAGE_ID_CHOOSE_EXECUTABLE		1
+#define QMC2_SETUPWIZARD_PAGE_ID_PROBE_EXECUTABLE		2
+#define QMC2_SETUPWIZARD_PAGE_ID_IMPORT_MAME_INI		3
+#define QMC2_SETUPWIZARD_PAGE_ID_ADJUST_SEARCH_PATHS		4
+#define QMC2_SETUPWIZARD_PAGE_ID_SETUP_COMPLETE			5
 
 class SetupWizard : public QWizard, public Ui::SetupWizard
 {
@@ -12,10 +19,17 @@ class SetupWizard : public QWizard, public Ui::SetupWizard
        	public:
 		SetupWizard(QSettings *cfg, QWidget *parent = 0);
 
+		QString &findMameIni();
+
 	public slots:
+
+	protected:
+		int nextId() const;
+		bool validateCurrentPage();
 
 	private:
 		QSettings *m_startupConfig;
+		QString m_mameIniPath;
 };
 
 #endif
