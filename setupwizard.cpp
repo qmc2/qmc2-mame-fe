f864abe15666cbc225e19d427c8e89df33455981
renereucher <renereucher@b01c4bed-ffd9-457b-aa39-baf6bcc640f5>
2017-06-13 16:06:44 +0000

    wip: more SetupWizard updates

--- setupwizard.cpp
+++ setupwizard.cpp
@@ -1,5 +1,10 @@
+#include <QFileDialog>
+#include <QStringList>
+#include <QLineEdit>
+#include <QFileInfo>
+#include <QTimer>
+
 #include "setupwizard.h"
-#include "macros.h"
 
 SetupWizard::SetupWizard(QSettings *cfg, QWidget *parent) :
 	QWizard(parent),
@@ -7,6 +12,37 @@ SetupWizard::SetupWizard(QSettings *cfg, QWidget *parent) :
 {
 	setupUi(this);
 	adjustSize();
+	connect(comboBoxExecutableFile->lineEdit(), SIGNAL(textChanged(const QString &)), this, SLOT(comboBoxExecutableFile_textChanged(const QString &)));
+	QTimer::singleShot(0, this, SLOT(init()));
+}
+
+void SetupWizard::init()
+{
+	button(QWizard::NextButton)->setEnabled(false);
+	QStringList emuHistory(m_startupConfig->value(QMC2_FRONTEND_PREFIX + "Welcome/EmuHistory", QStringList()).toStringList());
+	emuHistory.sort();
+	for (int i = 0; i < emuHistory.count(); i++) {
+		QString emuPath(emuHistory.at(i));
+		QFileInfo fi(emuPath);
+		if ( fi.exists() && fi.isReadable() && fi.isExecutable() && fi.isFile() )
+			comboBoxExecutableFile->insertItem(i, emuPath);
+	}
+	comboBoxExecutableFile->lineEdit()->setText(m_startupConfig->value(QMC2_EMULATOR_PREFIX + "FilesAndDirectories/ExecutableFile", QString()).toString());
+	int index = comboBoxExecutableFile->findText(comboBoxExecutableFile->lineEdit()->text());
+	if ( index >= 0 )
+		comboBoxExecutableFile->setCurrentIndex(index);
+}
+
+void SetupWizard::probeExecutable()
+{
+	button(QWizard::NextButton)->setEnabled(false);
+	QFileInfo fi(comboBoxExecutableFile->currentText());
+	if ( fi.isReadable() && fi.isExecutable() )
+		labelFileIsExecutableResult->setText("<font color=\"green\" size=\"+1\"><b>" + tr("Yes!") + "</b></font>");
+	else
+		labelFileIsExecutableResult->setText("<font color=\"red\" size=\"+1\"><b>" + tr("No!") + "</b></font>");
+	// FIXME
+	button(QWizard::NextButton)->setEnabled(true);
 }
 
 int SetupWizard::nextId() const
@@ -28,21 +64,29 @@ int SetupWizard::nextId() const
 	}
 }
 
+void SetupWizard::initializePage(int id)
+{
+	switch ( id ) {
+		case QMC2_SETUPWIZARD_PAGE_ID_PROBE_EXECUTABLE:
+			labelFileIsExecutableResult->setText("<font size=\"+1\">" + tr("Check result pending...") + "</font>");
+			labelIdentifiedAsMameResult->setText("<font size=\"+1\">" + tr("Check result pending...") + "</font>");
+			labelVersionSupportedResult->setText("<font size=\"+1\">" + tr("Check result pending...") + "</font>");
+			labelTotalMachinesResult->setText("<font size=\"+1\">" + tr("Check result pending...") + "</font>");
+			labelBinaryIdentHashResult->setText("<font size=\"+1\">" + tr("Check result pending...") + "</font>");
+			labelFileModificationDateResult->setText("<font size=\"+1\">" + tr("Check result pending...") + "</font>");
+			QTimer::singleShot(0, this, SLOT(probeExecutable()));
+			break;
+	}
+}
+
 bool SetupWizard::validateCurrentPage()
 {
 	switch ( currentId() ) {
-		case QMC2_SETUPWIZARD_PAGE_ID_CHOOSE_EXECUTABLE:
-			return !comboBoxExecutableFile->currentText().isEmpty();
 		case QMC2_SETUPWIZARD_PAGE_ID_PROBE_EXECUTABLE:
 			findMameIni();
-			return true;
-		case QMC2_SETUPWIZARD_PAGE_ID_IMPORT_MAME_INI:
-			return true;
-		case QMC2_SETUPWIZARD_PAGE_ID_ADJUST_SEARCH_PATHS:
-			return true;
-		default:
-			return true;
+			break;
 	}
+	return true;
 }
 
 QString &SetupWizard::findMameIni()
@@ -50,4 +94,23 @@ QString &SetupWizard::findMameIni()
 	m_mameIniPath.clear();
 	// FIXME
 	return m_mameIniPath;
+}
+
+void SetupWizard::comboBoxExecutableFile_textChanged(const QString &text)
+{
+	QFileInfo fi(text);
+	button(QWizard::NextButton)->setEnabled(fi.exists() && fi.isFile());
+}
+
+void SetupWizard::on_toolButtonBrowseExecutableFile_clicked()
+{
+	QString s;
+	if ( comboBoxExecutableFile->lineEdit()->text().isEmpty() )
+		s = QFileDialog::getOpenFileName(this, tr("Choose emulator executable file"), QString(), tr("All files (*)"), 0, useNativeFileDialogs() ? (QFileDialog::Options)0 : QFileDialog::DontUseNativeDialog);
+	else {
+		QFileInfo fileInfo(comboBoxExecutableFile->lineEdit()->text());
+		s = QFileDialog::getOpenFileName(this, tr("Choose emulator executable file"), fileInfo.absoluteFilePath(), tr("All files (*)"), 0, useNativeFileDialogs() ? (QFileDialog::Options)0 : QFileDialog::DontUseNativeDialog);
+	}
+	if ( !s.isNull() )
+		comboBoxExecutableFile->lineEdit()->setText(s);
 }
